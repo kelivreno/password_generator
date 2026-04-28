e3212e37299311c05d232293f6878b059f228e68
Kelangit Hakim <90366335+kelivreno@users.noreply.github.com>
2026-04-28 01:20:51 -0700

    Update password generation logic and button IDs for clarity

--- index.js
+++ index.js
@@ -6,15 +6,28 @@ let pwdElTwo = document.getElementById("pwd-el-two");
 const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"]
 
+let count = 0
+
 function genPassword(){
     pwdNew = "";
-    
-    for (let i = 0; i < 15; i++ ) {
-        randomI= Math.floor(Math.random()*characters.length)
-        pwdNew += characters[randomI]
-    }
+    count++
 
-    pwdElOne.textContent = pwdNew
+    if (count == 1) {
+        for (let i = 0; i < 15; i++ ) {
+            randomI= Math.floor(Math.random()*characters.length)
+            pwdNew += characters[randomI]
+        }
+
+        pwdElOne.textContent = pwdNew
+
+    } else if (count == 2) {
+        for (let i = 0; i < 15; i++ ) {
+            randomI= Math.floor(Math.random()*characters.length)
+            pwdNew += characters[randomI]
+        }
+
+        pwdElTwo.textContent = pwdNew
+    }
    
 }
 
